a7caad50fc132769dbebaa4da3afd1b37ceeee73
Relu <dum.relu@gmail.com>
2017-12-26 17:16:39 +0200

    Bounding Box volume

---
+++ include/bounding_box.hpp
@@ -0,0 +1,67 @@
+#pragma once
+
+#include <algorithm>
+
+namespace ppc
+{
+	template <typename T>
+	struct point
+	{
+		using scalar_type = T;
+
+		scalar_type x;
+		scalar_type y;
+		scalar_type z;
+
+		template <typename U>
+		bool operator==(const point<U>& other) const
+		{
+			return x == other.x
+				&& y == other.y
+				&& z == other.z;
+		}
+
+		template <typename U>
+		bool operator!=(const point<U>& other) const
+		{
+			return !(*this == other);
+		}
+	};
+
+	template <typename T>
+	struct bounding_box
+	{
+		using scalar_type = T;
+		using point_type = point<scalar_type>;
+
+		point_type min{};
+		point_type max{};
+	};
+
+	template <typename T>
+	void expand(bounding_box<T>& box, const typename bounding_box<T>::point_type& point)
+	{
+		const auto& min = box.min;
+		const auto& max = box.max;
+
+		box.min = { std::min(min.x, point.x), std::min(min.y, point.y), std::min(min.z, point.z) };
+		box.max = { std::max(max.x, point.x), std::max(max.y, point.y), std::max(max.z, point.z) };
+	}
+
+	template <typename T>
+	void expand(bounding_box<T>& box1, const bounding_box<T>& box2)
+	{
+		expand(box1, box2.min);
+		expand(box1, box2.max);
+	}
+
+	template <typename T>
+	bool intersects(const bounding_box<T>& box1, const bounding_box<T>& box2)
+	{
+		bool noIntersection =
+			box1.max.x < box2.min.x || box1.min.x > box2.max.x ||
+			box1.max.y < box2.min.y || box1.min.y > box2.max.y ||
+			box1.max.z < box2.min.z || box1.min.z > box2.max.z;
+		return !noIntersection;
+	}
+}
--- test/unit_tests.cpp
+++ test/unit_tests.cpp
@@ -1,7 +1,54 @@
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
 
-TEST_CASE("Hello World!", "[hello]")
+#include "bounding_box.hpp"
+
+using bbox = ppc::bounding_box<int>;
+using point = bbox::point_type;
+
+TEST_CASE("Bounding box initialization", "[bbox]")
 {
-    REQUIRE(true);
+	point min{ 0, 0, 0 };
+	point max{ 10, 10, 10 };
+	point center{ 5, 5, 5 };
+	bbox box{ min, max };
+
+	REQUIRE(box.min == min);
+	REQUIRE(box.max == max);
+}
+
+TEST_CASE("Expand a bounding box with a point", "[expand]")
+{
+	bbox box{ { 0, 0, 0 },{ 10, 10, 10 } };
+
+	point toContain{ -1, 15, 5 };
+	ppc::expand(box, toContain);
+
+	REQUIRE(box.min == point{ -1, 0, 0 });
+	REQUIRE(box.max == point{ 10, 15, 10 });
+}
+
+TEST_CASE("Expand a bounding box with another bounding box", "[expand]")
+{
+	bbox box1{ { 0, 0, 0 },{ 10, 10, 10 } };
+	bbox box2{ { 5, -1, 5 },{ 15, 9, 12 } };
+
+	ppc::expand(box1, box2);
+
+	REQUIRE(box1.min == point{ 0, -1, 0 });
+	REQUIRE(box1.max == point{ 15, 10, 12 });
+}
+
+TEST_CASE("Intersection between two bounding boxes", "[intersection]")
+{
+	bbox box{ { 0, 0, 0 },{ 10, 10, 10 } };
+	bbox box1{ { 5, 5, 5 },{ 12, 13, 14 } };
+	bbox box2{ { 2, 2, 2 },{ 7, 7, 7 } };
+	bbox box3{ { 10, 10, 10 },{ 11, 12, 13 } };
+	bbox box4{ { -5, -6, -7 },{ -1, -1, -1 } };
+
+	REQUIRE(ppc::intersects(box, box1));
+	REQUIRE(ppc::intersects(box, box2));
+	REQUIRE(ppc::intersects(box, box3));
+	REQUIRE(!ppc::intersects(box, box4));
 }
